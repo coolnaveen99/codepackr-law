212dbaea50c0608c80ff49d14cbccbbeabf427d0
Naveen <coolnaveen99@outlook.com>
2026-09-25 20:16:52 -0400

    feat(mobile): import mobile-tokens.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './data/liveSubjects'
 import App from './App'
 import './index.css'
+import './mobile-tokens.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
